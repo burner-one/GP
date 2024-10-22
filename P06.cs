88d20263d89150a0d3b1acad03e27cf157961483
jack napier <freddiequellfreddie@gmail.com>
2024-10-23 00:37:31 +0530

    Update P06.cs

--- P06.cs
+++ P06.cs
@@ -1,32 +1,42 @@
-using System.Collections; using 
-System.Collections.Generic; 
-using UnityEngine;  
-  
-public class camerashake : MonoBehaviour { 
-public Transform cameraTransform; private 
-Vector3 _originalPosOfCam; public float 
-shakeFrequency;   
-initialization   void Start () {  
-    _originalPosOfCam = cameraTransform.position;  
-  }  
-    
- void Update () {     if 
-(Input.GetKey(KeyCode.S))  
-    {  
-       CameraShake();  
-    }  
-    else if(Input.GetKeyUp(KeyCode.S))  
-    {  
-      StopShake();  
-    }     
-}  
-private void CameraShake()  
-{  
-  cameraTransform.position = _originalPosOfCam +  
-Random.insideUnitSphere*shakeFrequency;  
-}  
-private void StopShake()  
-{  
-  cameraTransform.position = _originalPosOfCam;  
-}  
-}  
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Transform cameraTransform; // Reference to the camera's transform
+    private Vector3 _originalPosOfCam; // Original position of the camera
+    public float shakeFrequency; // Frequency of the shake
+
+    // Initialization
+    void Start()
+    {
+        _originalPosOfCam = cameraTransform.position; // Store the original position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for shake input
+        if (Input.GetKey(KeyCode.S))
+        {
+            CameraShake(); // Start shaking
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            StopShake(); // Stop shaking
+        }
+    }
+
+    // Apply camera shake
+    private void CameraShake()
+    {
+        cameraTransform.position = _originalPosOfCam + Random.insideUnitSphere * shakeFrequency;
+    }
+
+    // Reset camera position
+    private void StopShake()
+    {
+        cameraTransform.position = _originalPosOfCam;
+    }
+}
