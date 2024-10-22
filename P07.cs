2acb47a41f479018c828bf3b486a2bb35c31950d
jack napier <freddiequellfreddie@gmail.com>
2024-10-23 00:38:10 +0530

    Update P07.cs

--- P07.cs
+++ P07.cs
@@ -1,23 +1,29 @@
-using System.Collections; using 
-System.Collections.Generic; 
-using UnityEngine;  
-  
-public class animate : MonoBehaviour {  
-public float speed;  
-Rigidbody2D rb2d;  
-  
-  
-  // Use this for initialization  
- void Start () {  
-    rb2d = GetComponent<Rigidbody2D>();  
-  }  
-    
-  // Update is called once per frame   void FixedUpdate () {  
-   float moveHorizontal = Input.GetAxis("Horizontal");  
-    float moveVertical = Input.GetAxis("Vertical");  
-    var movement = new  
-Vector2(moveHorizontal,moveVertical).normalized*speed*Time.deltaTime;  
-      
-    rb2d.AddForce(movement);  
-  }  
-} 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    public float speed; // Speed of the object
+    private Rigidbody2D rb2d; // Rigidbody component
+
+    // Initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+    }
+
+    // Update is called once per physics frame
+    void FixedUpdate()
+    {
+        // Get input for movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create movement vector
+        var movement = new Vector2(moveHorizontal, moveVertical).normalized * speed * Time.deltaTime;
+
+        // Apply force to the Rigidbody2D
+        rb2d.AddForce(movement);
+    }
+}
