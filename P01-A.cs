e28e990cad2c96dc4adda376507be946c355dc21
jack napier <freddiequellfreddie@gmail.com>
2024-10-23 00:42:21 +0530

    Update P01-A.cs

--- P01-A.cs
+++ P01-A.cs
@@ -1,38 +1,39 @@
-using System; 
-using System.Collections.Generic; 
-using System.ComponentModel; 
-using System.Data; 
-using System.Drawing; 
-using System.Text; 
-using System.Windows.Forms; 
-using Microsoft.DirectX; 
-using Microsoft.DirectX.Direct3D; 
-namespace Prac1a 
-{ 
-public partial class Form1 : Form 
-{ 
-Microsoft.DirectX.Direct3D.Device device; 
-public Form1() 
-{ 
-InitializeComponent(); 
-InitDevice(); 
-} 
-public void InitDevice() 
-{ 
-PresentParameters pp = new PresentParameters(); 
-pp.Windowed = true; 
-pp.SwapEffect = SwapEffect.Discard; 
-device = new Device(0, DeviceType.Hardware, this, 
-CreateFlags.HardwareVertexProcessing, pp); 
-} 
-public void Render() 
-{ 
-device.Clear(ClearFlags.Target, Color.Orange, 0, 1); 
-device.Present(); 
-} 
-private void Form1_Paint(object sender, PaintEventArgs e) 
-{ 
-Render(); 
-} 
-} 
-} 
+using System;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace Prac1a
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.DirectX.Direct3D.Device device; // Direct3D device
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitDevice(); // Initialize the device
+        }
+
+        public void InitDevice()
+        {
+            PresentParameters pp = new PresentParameters
+            {
+                Windowed = true, // Windowed mode
+                SwapEffect = SwapEffect.Discard // Discard swap effect
+            };
+            device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing, pp);
+        }
+
+        public void Render()
+        {
+            device.Clear(ClearFlags.Target, Color.Orange, 0, 1); // Clear screen
+            device.Present(); // Present the back buffer
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render(); // Render on paint
+        }
+    }
+}
